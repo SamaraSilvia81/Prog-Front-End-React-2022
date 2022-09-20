f92ea9ab220c19ca67eb4f533702932530e9b70a
aluno <aluno0@unicap.br>
2022-09-20 18:17:53 -0300

    update files

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import './App.css';
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import CollectionRomance from './components/CollectionRomance';
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const genreBooks = [
+  'Romance',
+  'Fantasia',
+  'Suspense',
+  'Aventura',
+  'Ficção Científica',
+];
+
+function ComboBox() {
+
+  const [value, setValue] = React.useState(genreBooks[-1]);
+  const [inputValue, setInputValue] = React.useState('');
+
+
+  return (
+
+    <div className='geral'>
+
+      <Box sx={{ flexGrow:1}}>
+        <AppBar position="static" sx={{ height:100}} >
+          <Toolbar className='NavBar'>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2, height:100}}
+            >
+            <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              LIBRARY STUDIO X
+            </Typography>
+
+            <Search>
+              <Autocomplete 
+                className='mySearch'
+                disablePortal
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+                inputValue={inputValue}
+                onInputChange={(event, newInputValue) => {
+                  setInputValue(newInputValue);
+                }}
+                id="combo-box-demo"
+                options={genreBooks}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Movie" />}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      {/* Quando eu coloco {value} não desaparece, mas tbm não rola.
+      setValue(top100Films[1])
+      */}
+
+      <div className='Cards'> 
+
+        { value !== genreBooks[0] ? null : (
+          <CollectionRomance/>
+        )}
+        
+      </div>
+    </div>
+
+  );
+}
+
+export default ComboBox
---
+++ src/components/CollectionRomance.js
@@ -0,0 +1,221 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+//import AmorGelato from './AmorGelato'
+
+function CollectionRomance (){
+
+    return (
+
+      <div>
+
+<div className='cards'>
+                <Card className='cardGeral' sx={{ maxWidth: 345}}>
+                    
+                    {/*Amor e Gelato */}
+                    <CardMedia
+                        component="img"
+                        height="400"
+                        image="https://4.bp.blogspot.com/-5oRVOOtTK0k/WdJOJVpC4JI/AAAAAAAAGJ4/hHj2htGCY-0d03QRWTfkfakYQFcY3AXfgCLcBGAs/s1600/amor%2Be%2Bgelato.png"
+                        alt="green iguana"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Amor e Gelato
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Um verão na Itália, uma antiga história de amor e um segredo de família   Depois da morte da mãe, Lina fica com a missão de realizar um último pedido: ir até a Itália para conhecer o pai.
+                    </Typography>
+                    </CardContent>
+
+                    <CardActions >
+                    <Button size="small">Download </Button>
+                    <Button size="small">Reservar</Button>
+                    <Button size="small">Learn More</Button>
+                    </CardActions>
+
+                </Card>
+
+        <Card className='cardGeral' sx={{ maxWidth: 345}}>
+            
+                {/*A culpa é das estrelas */}
+                <CardMedia
+                component="img"
+                height="400"
+                image="https://www.intrinseca.com.br/blog/wp-content/uploads/2017/04/3IMG_1027-1024x1024.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                A Culpa é das Estrelas
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                    Narra o romance de dois adolescentes que se conhecem (e se apaixonam) em um Grupo de Apoio para Crianças com Câncer.
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Download </Button>
+                <Button size="small">Reservar</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+
+        <Card className='cardGeral' sx={{ maxWidth: 345}}>
+            
+            {/*A cinco passos de você */}
+            <CardMedia
+                component="img"
+                height="400"
+                image="https://i.pinimg.com/originals/1f/16/71/1f16717be50844d70a783488dac78cd6.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                A Cinco Passos de Você
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                    É uma história adolescente que valoriza a família, a lealdade entre amigos, o poder e a importância do amor e aumenta a conscientização sobre a fibrose cística. 
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Download </Button>
+            <Button size="small">Reservar</Button>
+            <Button size="small">Learn More</Button>
+            </CardActions>
+            </Card>
+
+            <Card className='cardGeral' sx={{ maxWidth: 345}}>
+                
+                    {/*Teto para Dois */}
+                    <CardMedia
+                    component="img"
+                    height="400"
+                    image="https://1.bp.blogspot.com/-iZJEJ1jz-HA/X0vqFfShTFI/AAAAAAAACpg/mCnwdEXMOZMp9PhMyc7B5LwLcrIkZwXfwCNcBGAsYHQ/s2048/IMG_8261.JPG"
+                    alt="green iguana"
+            />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Teto para Dois
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Eles dividem um apartamento com uma cama só. Ele dorme de dia, ela, à noite. Os dois nunca se encontraram, mas estão prestes a descobrir que, para se sentir em casa, às vezes é preciso jogar as regras pela janela.
+                </Typography>
+                </CardContent>
+                <CardActions>
+                <Button size="small">Download </Button>
+                <Button size="small">Reservar</Button>
+                <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>  
+        </div>
+
+        <div className='cards'>
+            <Card className='cardGeral' sx={{ maxWidth: 345}}>
+                
+                {/*Eleanor e Park */}
+                <CardMedia
+                    component="img"
+                    height="400"
+                    image="https://4.bp.blogspot.com/-5oRVOOtTK0k/WdJOJVpC4JI/AAAAAAAAGJ4/hHj2htGCY-0d03QRWTfkfakYQFcY3AXfgCLcBGAs/s1600/amor%2Be%2Bgelato.png"
+                    alt="green iguana"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Eleanor e Park
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Um verão na Itália, uma antiga história de amor e um segredo de família   Depois da morte da mãe, Lina fica com a missão de realizar um último pedido: ir até a Itália para conhecer o pai.
+                </Typography>
+                </CardContent>
+
+                <CardActions>
+                    <Button size="small">Download </Button>
+                    <Button size="small">Reservar</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+                    
+            </Card>
+
+            <Card className='cardGeral' sx={{ maxWidth: 345}}>
+
+            {/*Querido Jhon*/}
+            <CardMedia
+            component="img"
+            height="400"
+            image="https://1.bp.blogspot.com/-FW_xhEK7fns/WkOWvYRgsrI/AAAAAAAAT8k/pIke0PYk8LU4MrQOwMolRT5j83OLIEPxwCLcBGAs/s1600/querido%2Bjohn%2Blivro.jpg"
+            alt="green iguana"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Querido Jhon
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Durante sete tumultuosos anos, o casal acaba separado pelas missões cada vez mais perigosas de John. 
+                Apesar de se encontrarem esporadicamente, John e Savannah ainda mantém contato por meio de cartas de amor, que acabam por provocar uma situação de graves conseqüências para ambos.
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Download </Button>
+            <Button size="small">Reservar</Button>
+            <Button size="small">Learn More</Button>
+            </CardActions>
+            </Card>
+
+            <Card className='cardGeral' sx={{ maxWidth: 345}}>
+
+            {/*Um porto Seguro */}
+            <CardMedia
+            component="img"
+            height="400"
+            image="https://1.bp.blogspot.com/-JfR9Zx3KlsI/WLnkPJIuCAI/AAAAAAAARk4/U78mZglmyrkpgLp1MLURDX_xzE9Nsib8ACLcB/s1600/IMG_4224.JPG"
+            alt="green iguana"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Um Porto Seguro
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Após uma briga doméstica, Katie foge de sua casa toda ensanguentada. Ela é perseguida pela polícia, mas consegue escapar e passa a viver num pequenina cidade no litoral da costa leste dos Estados Unidos.
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Download </Button>
+            <Button size="small">Reservar</Button>
+            <Button size="small">Learn More</Button>
+            </CardActions>
+            </Card>
+
+            <Card className='cardGeral' sx={{ maxWidth: 345}}>
+
+                {/*O melhor de mim*/}
+                <CardMedia
+                component="img"
+                height="400"
+                image="https://3.bp.blogspot.com/-bS4-gwjlQh0/VsCqNi1yEuI/AAAAAAAAQfU/ofzcdvHJPlE/s1600/O%2Bmelhor%2Bde%2Bmim.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                O melhor de mim
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                 Dawson e Amanda eram adolescentes quando se conheceram no início dos anos 1990 numa cidadezinha perto de Nova Orleans.
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Download </Button>
+            <Button size="small">Reservar</Button>
+            <Button size="small">Learn More</Button>
+            </CardActions>
+            </Card>
+        </div>
+
+
+      </div>
+
+)}
+
+export default CollectionRomance
